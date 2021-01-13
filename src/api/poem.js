3380a51ed3a7f0d67dd2a7d7cffbdd250553e059
光伟 <yemingrujing@gmail.com>
2021-01-13 18:56:35 +0800

    一言

--- src/api/poem.js
+++ src/api/poem.js
@@ -1,9 +1,17 @@
 import Request from '@/utils/request';
 
 export function search(data) {
-  return Request.post('/poem/search', data);
+  return Request.post('/api/poem/search', data);
+}
+
+export function add(data) {
+  return Request.post('/api/poem/add', data);
+}
+
+export function edit(data) {
+  return Request.post('/api/poem/edit', data);
 }
 
 export function del(data) {
-  return Request.post('/poem/del', data);
+  return Request.post('/api/poem/del', data);
 }
--- src/views/blog/poem.vue
+++ src/views/blog/poem.vue
@@ -1,8 +1,37 @@
 <template>
   <div class="app-container">
-    <el-select ref="role" v-model="listQuery.type_name" clearable class="filter-item" placeholder="选择类型">
-      <el-option v-for="(v, k,i) in types" :key="i" :label="v" :value="k" />
-    </el-select>
+    <div class="filter-container">
+      <el-select ref="role" v-model="listQuery.typeName" clearable class="filter-item" placeholder="选择类型">
+        <el-option v-for="(v, k,i) in types" :key="i" :label="v" :value="k" />
+      </el-select>
+      <el-button type="primary" class="filter-item" @click="add">添加</el-button>
+    </div>
+    <el-dialog :title="title" :visible.sync="alterVisible" width="500px">
+      <el-form ref="poem" :model="form" label-width="80px" label-position="left">
+        <el-form-item label="类型" prop="typeName">
+          <el-select ref="typeName" v-model.trim="form.typeName" clearable class="filter-item" placeholder="选择类型">
+            <el-option v-for="(v, k, i) in types" :key="i" :label="v" :value="k" />
+          </el-select>
+        </el-form-item>
+        <el-form-item ref="author" label="来源" prop="author">
+          <el-input v-model.trim="form.author" placeholder="请输入来源" />
+        </el-form-item>
+        <el-form-item label="内容" prop="content">
+          <el-input
+            ref="content"
+            v-model.trim="form.content"
+            type="textarea"
+            :rows="4"
+          />
+        </el-form-item>
+        <span slot="footer" class="dialog-footer">
+          <el-button @click="alterVisible = false">取 消</el-button>
+          <el-button type="primary" @click="ok()">确 定</el-button></span>
+      </el-form>
+      <span slot="footer" class="dialog-footer">
+        <el-button @click="alterVisible = false">取 消</el-button>
+        <el-button type="primary" @click="ok()">确 定</el-button></span>
+    </el-dialog>
     <!-- 表格区域 -->
     <nice-table
       :table-header="tableHeader"
@@ -18,7 +47,7 @@
 </template>
 
 <script>
-import { del, search } from '@/api/poem';
+import { del, search, add, edit } from '@/api/poem';
 
 export default {
   name: 'Poem',
@@ -26,8 +55,11 @@ export default {
     return {
       list: [],
       loading: true,
-      listQuery: { page: 1, type_name: '', limit: 10 },
+      form: {},
+      alterVisible: false,
+      listQuery: { page: 1, typeName: '', limit: 10 },
       total: 0,
+      title: '',
       types: {
         a: '动画',
         b: '漫画',
@@ -44,17 +76,20 @@ export default {
       },
       type: '',
       tableHeader: [
-        { field: 'poem', title: '内容' },
-        { field: 'create_time', title: '创建时间' },
-        { field: 'type_name', title: '类别', formatter: 'type_name' },
+        { field: 'content', title: '内容' },
+        { field: 'createTime', title: '创建时间' },
+        { field: 'typeName', title: '类别', formatter: 'type_name' },
         { field: 'author', title: '来源' },
         { field: 'toolbar', title: '操作' }
       ],
-      toolbarList: [{ title: '删除', field: 'handleDel', type: 'danger' }]
+      toolbarList: [
+        { title: '编辑', field: 'handleEdit', type: 'primary' },
+        { title: '删除', field: 'handleDel', type: 'danger' }
+      ]
     };
   },
   watch: {
-    'listQuery.type_name': {
+    'listQuery.typeName': {
       handler() {
         this.search();
       },
@@ -81,6 +116,33 @@ export default {
       });
     },
     stateSearch() {
+    },
+    add() {
+      this.alterVisible = true;
+      this.form = { content: '', typeName: '', author: '' };
+      this.title = '新增一言';
+    },
+    ok() {
+      if (!this.form.content || !this.form.typeName || !this.form.author) {
+        return;
+      }
+      this.form.id ? edit(this.form).then(res => {
+        this.search();
+        this.alterVisible = false;
+      }) : add(this.form).then(res => {
+        this.search();
+        this.alterVisible = false;
+      });
+    },
+    handleEdit(data) {
+      this.title = '编辑标签';
+      this.alterVisible = true;
+      this.form = {
+        content: data.content,
+        typeName: data.typeName,
+        author: data.author,
+        id: data.id
+      };
     },
     handleDel(data) {
       this.loading = true;
