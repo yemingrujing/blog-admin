aeed7d3e1139bdde92e7c114b8241ae6dfb1ad4e
光伟 <yemingrujing@gmail.com>
2021-01-15 18:32:36 +0800

    相册管理

--- vue.config.js
+++ vue.config.js
@@ -31,7 +31,7 @@ module.exports = {
         }
       },
       '/image-base-url': {
-        target: 'https://raw.githubusercontent.com/yemingrujing/Drawing_Images/main',
+        target: 'https://raw.githubusercontent.com/yemingrujing/Drawing_Images/main/',
         changeOrigin: true,
         pathRewrite: {
           '^/image-base-url': ''
