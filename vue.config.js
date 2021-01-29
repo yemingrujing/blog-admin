c0d0687de32ed7cad28c4a5eb38dc4bbe29ddaa6
光伟 <yemingrujing@gmail.com>
2021-01-29 10:04:14 +0800

    优化

--- vue.config.js
+++ vue.config.js
@@ -19,7 +19,7 @@ module.exports = {
   productionSourceMap: false,
   runtimeCompiler: true,
   devServer: {
-    host: '0.0.0.0',
+    host: '127.0.0.1',
     port: defaultSettings.port,
     open: false,
     overlay: {
